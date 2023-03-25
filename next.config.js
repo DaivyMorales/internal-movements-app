9c8aa2ddd0f7d4ee663fd6ceea54a9d30dcad2c3
Daivy Morales <daivymorales7705@gmail.com>
2023-03-25 10:55:07 -0500

    Remove /src in config CORS

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/src/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
