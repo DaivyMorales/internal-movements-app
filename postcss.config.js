19437daa9084d501c223af2dea4bd294e8147c3f
Daivy Morales <daivymorales7705@gmail.com>
2023-03-24 19:24:37 -0500

    nose

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
 };
-=======
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 8c472f252fb581660257381eda69455d4c9b1ed7
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,8 @@
 import ProductContextComponent from "@/context/ContextProducts";
 import ContextInformationComponent from "@/context/ContextInformation";
 import "@/styles/globals.css";
-<<<<<<< HEAD
 import { Inter } from "@next/font/google";
 import "../../styles/main.css";
-=======
-import { Inter } from '@next/font/google';
->>>>>>> 8c472f252fb581660257381eda69455d4c9b1ed7
 
 export default function App({ Component, pageProps }) {
   return (
